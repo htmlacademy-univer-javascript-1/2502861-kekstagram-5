3f8a1cae59e1608ed92927458ad368f31509e5b5
Легков Никита <nik.legkov@mail.ru>
2024-12-23 15:10:33 +0500

    Создаст валидацию формы

--- js/utils.js
+++ js/utils.js
@@ -1,42 +1,25 @@
-// Генератор уникальных айди (порядковые числа, начиная с)
-const createIdGenerator = (start = 0) => () => {
-  start += 1;
-  return start;
-};
-
-
-// Ф-ция, кот. генерирует случайное положит число из диапазона
-
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-// Ф-ция, кот. генерирует случайный уникальный айди из диапазона
+const getRandomObjectArray = (builder, max) => Array.from({ length: getRandomNumber(0, max) }, builder);
 
-const createRandomIdFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= max - min + 1) {
-      return null;
+function createIdGenerator(min, max) {
+  let lastGeneratedId = min;
+
+  return function () {
+    lastGeneratedId += 1;
+    if (lastGeneratedId > max) {
+      lastGeneratedId = min;
     }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
+
+    return lastGeneratedId;
   };
-};
+}
 
-const getRandomArrayElement = (elements) =>
-  elements[getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
 
-export {
-  createIdGenerator,
-  getRandomInteger,
-  createRandomIdFromRangeGenerator,
-  getRandomArrayElement,
-};
+export { getRandomNumber, getRandomObjectArray, createIdGenerator, getRandomArrayElement };
